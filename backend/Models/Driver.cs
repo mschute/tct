1e3c34d23ad9db0aac702f07a79ebe645fd83bac
mschute <46330682+mschute@users.noreply.github.com>
2024-04-04 18:48:13 +0100

    Updated TCT Models

--- backend/Models/Driver.cs
+++ backend/Models/Driver.cs
@@ -5,10 +5,10 @@ namespace backend.Models;
 public class Driver
 {
     public int DriverId { get; set; }
-    public string? FirstName { get; set; }
-    public string? LastName { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
     public DateOnly? Dob { get; set; }
-    public string? DrivingLicenseNo { get; set; }
+    public string DrivingLicenseNo { get; set; }
     
     [JsonIgnore]
     public ICollection<Booking>? Bookings { get; set; }
