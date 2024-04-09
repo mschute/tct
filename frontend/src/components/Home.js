18c3b0555c12125b625fb3de8b8d0b992dcc05ca
mschute <46330682+mschute@users.noreply.github.com>
2024-04-09 21:23:50 +0100

    Update Home.js
    Fixed bug in Home.js so it renders.

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -4,31 +4,34 @@ import Hero from "./Hero";
 import ContactForm from "./ContactForm";
 import Footer from "./Footer";
 
-const HomePage = () => (
-    <div>
-        <Header />
-        <main>
-            <Hero/>
-            <div>
-                <h2>
-                    Our Service
-                </h2>
-                <p>
-                    WORDS!
-                </p>
-            </div>
-            <div>
-                <h2>
-                    About us
-                </h2>
-                <p>
-                    WORDS!
-                </p>
-            </div>
-            <ContactForm/>
-        </main>
-        <Footer />
-    </div>
-)
 
-export default HomePage;
+const Home = () => {
+    return (
+        <>
+            <Header/>
+            <main>
+                <Hero/>
+                <div>
+                    <h2>
+                        Our Service
+                    </h2>
+                    <p>
+                        WORDS!
+                    </p>
+                </div>
+                <div>
+                    <h2>
+                        About us
+                    </h2>
+                    <p>
+                        WORDS!
+                    </p>
+                </div>
+                <ContactForm/>
+            </main>
+            <Footer/>
+        </>
+    );
+}
+
+export default Home;
