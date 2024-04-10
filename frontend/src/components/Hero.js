459030c51d7cda8e51987d27d1ab480496d1bbb8
mschute <46330682+mschute@users.noreply.github.com>
2024-04-10 10:22:49 +0100

    Main page sections
    Added the main page sections and some css styling

--- frontend/src/components/Hero.js
+++ frontend/src/components/Hero.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import {Container, Image} from "react-bootstrap";
+import '../styles/hero.css';
+import '../styles/global.css';
+
 
 const Hero = () => {
     return (
-        <Container fluid>
-            <Image
-                fluid
-                src="/images/PrayingHandsOfMaryGlenLyon.jpg" 
-                alt="Hero"
-                style={{ maxWidth: '100%', margin: 'auto' }}
-            />
+        <Container fluid className='p-0'>
+            <div className='logo-overlay'>
+                <Image src='../images/Logo.png' alt='logo' fluid/>
+                <h1 className='business-title'>Tay Country Travel</h1>
+                
+            </div>
+
         </Container>
 
     );
