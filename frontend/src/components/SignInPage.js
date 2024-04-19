8934a999effbdb5de345a829f8b3152e2db2677f
mschute <46330682+mschute@users.noreply.github.com>
2024-04-19 11:06:42 +0100

    Updated sign in sign up form to send message to verify email address

--- frontend/src/components/SignInPage.js
+++ frontend/src/components/SignInPage.js
@@ -9,75 +9,90 @@ import {useNavigate} from 'react-router-dom';
 const formTypes = {signIn: "Sign In", signUp: "Sign Up"}
 
 const SignInPage = ({isAuthenticated, setIsAuthenticated, handleSetJwtToken, handleSetUserRole}) => {
-        const [user, setUser] = useState({email: '', password: ''});
-        const [formType, setFormType] = useState(formTypes.signIn);
-        const navigation = useNavigate();
-        const [error, setError] = useState('');
+    const [user, setUser] = useState({email: '', password: ''});
+    const [formType, setFormType] = useState(formTypes.signIn);
+    const navigation = useNavigate();
+    const [error, setError] = useState('');
+    const [isButtonDisabled, setIsButtonDisabled] = useState(true);
+    const [verificationMessage, setVerificationMessage] = useState('');
 
-        const handleInputChange = (event) => {
-            const {name, value} = event.target
-            setUser({...user, [name]: value})
-        }
-
-        //TODO Need to add user input validation to make sure they have both a username or password
-        const handleSubmit = async (event) => {
-            event.preventDefault();
-            try {
-                if (formType === formTypes.signIn) {
-                    const response = await accountService.login(user);
-                    const token = response.token
-
-                    handleSetJwtToken(token);
-
-                    navigation('/');
-                } else {
-                    //TODO Need to add pop-up asking user to go to their email address to verify their email.
-                    await accountService.register(user);
-                    navigation('/');
-                }
-            } catch (error) {
-                setError(error.message || "An error occurred during login.");
-            }
-        };
-
-        const handleFormSwitch = () => {
-            setFormType(formType === formTypes.signIn ? formTypes.signUp : formTypes.signIn)
-        }
-
-        const title = formType === formTypes.signIn ? "Sign In" : "Sign Up";
-        const submitButtonText = formType === formTypes.signIn ? "Sign In" : "Sign Up";
-        const switchButtonText = formType === formTypes.signUp ? "Sign into your account" : "Create an account";
-
-        return (
-            <div className="page-dimensions">
-                <div className="form-container">
-
-
-                    <h4 className="title">{title}</h4>
-                    <form method="POST">
-                        <div className="sign-in-container">
-                            <div className="sign-in-field-container">
-                                <input className="sign-in-field" name="email" placeholder="Email Address"
-                                       onChange={handleInputChange}/>
-                            </div>
-                            <div className="sign-in-field-container">
-                                <input className="sign-in-field" name="password" type="password"
-                                       placeholder="Password"
-                                       onChange={handleInputChange}/>
-                            </div>
-                            <div className="button-container">
-                                <button className="primary-button" type="submit"
-                                        onClick={handleSubmit}>{submitButtonText}</button>
-                            </div>
-                        </div>
-                    </form>
-                    <div className="sign-in-container">
-                        <button className="text-link-button" onClick={handleFormSwitch}>{switchButtonText}</button>
-                    </div>
-                </div>
-            </div>
-        );
+    const handleInputChange = (event) => {
+        const {name, value} = event.target
+        setUser({...user, [name]: value})
+        setIsButtonDisabled(!(user.email && user.password));
     }
-;
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        try {
+            if (formType === formTypes.signIn) {
+                const response = await accountService.login(user);
+                const token = response.token
+
+                handleSetJwtToken(token);
+
+                navigation('/');
+            } else {
+                //TODO Need to add pop-up asking user to go to their email address to verify their email.
+                await accountService.register(user);
+                setVerificationMessage( "Registration successful. An email has been sent to verify your email address.");
+                setUser({email: '', password: ''});
+                navigation('/');
+            }
+            setError('');
+            setVerificationMessage('');
+        } catch (error) {
+            setError(error.message || "An error occurred during sign-in. Email and password combination not valid.");
+        }
+    };
+
+    const handleFormSwitch = () => {
+        setFormType(formType === formTypes.signIn ? formTypes.signUp : formTypes.signIn)
+    }
+
+    const title = formType === formTypes.signIn ? "Sign In" : "Sign Up";
+    const submitButtonText = formType === formTypes.signIn ? "Sign In" : "Sign Up";
+    const switchButtonText = formType === formTypes.signUp ? "Sign into your account" : "Create an account";
+
+    return (
+        <div className="page-dimensions">
+            <div className="form-container">
+                <h4 className="title">{title}</h4>
+                <form method="POST">
+                    <div className="sign-in-container">
+                        <div className="sign-in-field-container">
+                            <input className="sign-in-field" name="email" placeholder="Email Address"
+                                   onChange={(e) => {
+                                       handleInputChange(e);
+                                       setError('');
+                                   }}/>
+                        </div>
+                        <div className="sign-in-field-container">
+                            <input className="sign-in-field" name="password" type="password"
+                                   placeholder="Password"
+                                   onChange={(e) => {
+                                       handleInputChange(e);
+                                       setError('');
+                                   }}/>
+                        </div>
+                        <div className="button-container">
+                            <button className="primary-button" type="submit"
+                                    onClick={handleSubmit} disabled={isButtonDisabled}>{submitButtonText}</button>
+                        </div>
+                    </div>
+                </form>
+                <div className="sign-in-container">
+                    <button className="text-link-button" onClick={handleFormSwitch}>{switchButtonText}</button>
+                </div>
+                {error === '' ? "" :
+                    <div className="error-message">Sign-in failed. Email and password combination invalid.</div>}
+
+                {verificationMessage === true ? "" :
+                    <div className="registration-message">{verificationMessage}</div>
+                }
+            </div>
+        </div>
+    );
+};
 
 export default SignInPage
