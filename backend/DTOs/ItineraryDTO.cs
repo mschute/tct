a84c0dc48bb3926665d375f3c2638071d3a659a1
mschute <46330682+mschute@users.noreply.github.com>
2024-04-21 12:52:27 +0100

    Removed last database migration
    Removed setting end time to null for migration

--- backend/DTOs/ItineraryDTO.cs
+++ backend/DTOs/ItineraryDTO.cs
@@ -5,7 +5,7 @@ public class ItineraryDTO
     public int ItineraryId { get; set; }
     public DateOnly TripDate { get; set; }
     public TimeOnly TripStartTime { get; set; }
-    public TimeOnly? TripEndTime { get; set; }
+    public TimeOnly TripEndTime { get; set; }
     public string CustomerName { get; set; }
     public int PassengerCount { get; set; }
     public ICollection<string> LocationNames { get; set; }
